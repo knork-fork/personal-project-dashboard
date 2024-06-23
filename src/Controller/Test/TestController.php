49aa95b13001c0895579586361881c0f192d2c7e
Luka Knezic <luka@giscloud.com>
2024-06-23 23:46:08 +0200

    Add Twig

--- src/Controller/Test/TestController.php
+++ src/Controller/Test/TestController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Test;
 use App\Service\Test\TestService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 final class TestController extends AbstractController
 {
@@ -14,7 +15,21 @@ final class TestController extends AbstractController
     ) {
     }
 
-    public function test(): JsonResponse
+    public function test(): Response
+    {
+        return $this->render('test.html.twig', [
+            'page_title' => 'Test Page'
+        ]);
+    }
+
+    /*public function test(): JsonResponse
+    {
+        return new JsonResponse(
+            $this->testService->test()
+        );
+    }*/
+
+    public function testJson(): JsonResponse
     {
         return new JsonResponse(
             $this->testService->test()
