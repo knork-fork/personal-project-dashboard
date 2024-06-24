61ccce0df901d9c17af7b8ca0e3161fea710a0bc
Luka Knezic <luka@giscloud.com>
2024-06-24 02:13:07 +0200

    Style fixes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,13 +1,14 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Controller;
 
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+final class SecurityController extends AbstractController
 {
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -22,6 +23,6 @@ class SecurityController extends AbstractController
 
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
